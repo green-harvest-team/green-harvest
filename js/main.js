d61c9ee8ccfdec29dfdbef4f6a3438bd66bf8356
Levent KÖYBAŞI <leventkoybasi@hotmail.com>
2024-08-13 01:08:34 -0700

    section four desktop added

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@ const menuCloseButton = document.querySelector("#menu-close-button");
 
 menuOpenButton.addEventListener("click", () => {
   menuHamburger.classList.add("is-open");
+  document.body.classList.add("no-scroll");
 });
 
 menuCloseButton.addEventListener("click", () => {
   menuHamburger.classList.remove("is-open");
+  document.body.classList.remove("no-scroll");
 });
